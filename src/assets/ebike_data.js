bccaeb6ece6c3043f064fd773da72d7bdc69425d
HoraceZ <zhanghorace@gmail.com>
2022-11-14 21:43:14 -0500

    updated data with image_url, website, and archive fields

--- src/assets/ebike_data.js
+++ src/assets/ebike_data.js
@@ -2,6 +2,8 @@ export const data = {
   ebikes: [
     {
       id: 1,
+      archive: false,
+      archive: false,
       brand: 'Fucare',
       model: 'Gemini',
       style: ['cargo', 'utility', 'fat tire'],
@@ -29,10 +31,13 @@ export const data = {
       front_basket: 'optional',
       fenders: 'included',
       speeds: 7,
+      image_url: 'https://cdn.shopify.com/s/files/1/0587/2818/6015/products/FUCAREGEMINISTEP-THRUELECTRICCARGOBIKEmain_1800x1800.jpg?v=1658931714',
+      website: 'https://fucarebike.com/products/fucare-gemini-electric-bike',
       crank_set_teeth: 42
     },
     {
       id: 2,
+      archive: false,
       brand: 'Fucare',
       model: 'Gemini X',
       style: ['cargo', 'utility', 'fat tire'],
@@ -60,10 +65,13 @@ export const data = {
       front_basket: 'optional',
       fenders: 'included',
       speeds: 7,
+      image_url: 'https://cdn.shopify.com/s/files/1/0587/2818/6015/products/a2a62734f0200f5f5cfe6301b7a1bba6_1800x1800.jpg?v=1650121000',
+      website: 'https://fucarebike.com/products/fucare-gemini-x-electric-bike',
       crank_set_teeth: 42
     },
     {
       id: 3,
+      archive: false,
       brand: 'Ariel Rider',
       model: 'Kepler',
       style: ['commuter', 'fat tire'],
@@ -91,10 +99,13 @@ export const data = {
       front_basket: 'optional',
       fenders: 'included',
       speeds: 7,
+      image_url: 'https://cdn.shopify.com/s/files/1/0065/6549/4839/products/red_side_male_720x.png?v=1648613637',
+      website: 'https://arielrider.com/products/kepler',
       crank_set_teeth: 52
     },
     {
       id: 4,
+      archive: false,
       brand: 'Ariel Rider',
       model: 'X-Class 52V',
       style: ['fat tire'],
@@ -122,10 +133,13 @@ export const data = {
       front_basket: 'none',
       fenders: 'included',
       speeds: 7,
+      image_url: 'https://cdn.shopify.com/s/files/1/0065/6549/4839/products/redxclass52Vright_720x.jpg?v=1653771935',
+      website: 'https://arielrider.com/products/x-class-52v',
       crank_set_teeth: 48
     },
     {
       id: 4,
+      archive: false,
       brand: 'Ariel Rider',
       model: 'X-Class 52V Step-Thru',
       style: ['fat tire'],
@@ -153,10 +167,13 @@ export const data = {
       front_basket: 'none',
       fenders: 'included',
       speeds: 7,
+      image_url: 'https://cdn.shopify.com/s/files/1/0065/6549/4839/products/RedX-52StepThruproductphoto_720x.jpg?v=1653781450',
+      website: 'https://arielrider.com/products/x-class-52v-step-thru',
       crank_set_teeth: 48
     },
     {
       id: 5,
+      archive: false,
       brand: 'Ariel Rider',
       model: 'Grizzly - 52V Dual Motor',
       style: ['fat tire', 'dual motor'],
@@ -184,49 +201,44 @@ export const data = {
       front_basket: 'none',
       fenders: 'included',
       speeds: 7,
+      image_url: 'https://cdn.shopify.com/s/files/1/0065/6549/4839/products/02_4c30754c-0519-4c0e-8693-0fb287f40324_720x.jpg?v=1657853314',
+      website: 'https://arielrider.com/products/grizzly',
       crank_set_teeth: 52
     }
   ]
 }
 
 // {
-//   id: 1,
-//   basic: {
-//     brand: ,
-//     model: ,
-//     style: [],
-//     price: ,
-//     weight: ,
-//     weight_capacity_lbs: ,
-//     top_speed_mph: ,
-//     wheel_size_in: ,
-//     tire_width_in: ,
-//     colors: [],
-//     folding: ,
-//     step_thru: ,
-//   },
-//   electronics: {
-//     battery: {
-//       capacity_ah: ,
-//       voltage: ,
-//     },
-//     controller_a: ,
-//     motor_wattage: ,
-//     motor_brand: ,
-//     torque_max_nm: ,
-//     range: ,
-//     cruise_control: ,
-//   },
-//   bicycle_components: {
-//     brakes: {
-//       brand: ,
-//       type: ,
-//     },
-//     suspension: ["],
-//     rear_rack: , // included, optional, none
-//     front_basket: , // included, optional, none
-//     fenders: , // included, optional, none
-//     speeds: ,
-//     crank_set_teeth: ,
-//   }
+//   id: ,
+//   archive: ,
+//   brand: ,
+//   model: ,
+//   style: [],
+//   price: ,
+//   weight: ,
+//   weight_capacity_lbs: ,
+//   top_speed_mph: ,
+//   wheel_size_in: ,
+//   tire_width_in: ,
+//   colors: [], // 'color'
+//   folding: ,
+//   step_thru: ,
+//   battery_capacity_ah: ,
+//   battery_voltage: ,
+//   controller_a: ,
+//   motor_wattage: ,
+//   motor_brand: ,
+//   torque_max_nm: ,
+//   range: ,
+//   cruise_control: ,
+//   brake_brand: ,
+//   brake_type: ,
+//   suspension: [], //'front', 'rear', 'full'
+//   rear_rack: , // 'included', 'optional', 'none'
+//   front_basket: , // 'included', 'optional', 'none'
+//   fenders: , // 'included', 'optional', 'none'
+//   speeds: ,
+//   image_url: ,
+//   website: ,
+//   crank_set_teeth: ,
 // }
