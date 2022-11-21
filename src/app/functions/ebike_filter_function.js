02da37faa568013e62ed8a94d2c07926a98ce0aa
HoraceZ <zhanghorace@gmail.com>
2022-11-20 23:10:38 -0500

    updated DB ebike schema

--- src/app/functions/ebike_filter_function.js
+++ src/app/functions/ebike_filter_function.js
@@ -20,19 +20,12 @@ export const cleanSingleEbikeData = (ebike) => {
   delete cleanedEbike.image_url
   delete cleanedEbike.website
   cleanedEbike.colors = cleanedEbike.colors.join(', ')
+  cleanedEbike.accessories = cleanedEbike.accessories.join(', ')
   cleanedEbike.style = cleanedEbike.style.join(', ')
 
   if (cleanedEbike.torque_max_nm === -1) cleanedEbike.torque_max_nm = 'No Data'
 
   if (cleanedEbike.controller_a === -1) cleanedEbike.controller_a = 'No Data'
 
-  if (cleanedEbike.cruise_control) cleanedEbike.cruise_control = 'Yes'
-  else cleanedEbike.cruise_control = 'No'
-
-  if (cleanedEbike.folding) cleanedEbike.folding = 'Yes'
-  else cleanedEbike.folding = 'No'
-
-  if (cleanedEbike.foldstep_thruing) cleanedEbike.step_thru = 'Yes'
-  else cleanedEbike.step_thru = 'No'
   return cleanedEbike
 }
